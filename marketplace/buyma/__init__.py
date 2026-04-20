32a32c3f49ed559499f9348ac48237d3fcd617f8
ohnimnah <gksalsgh96@gmail.com>
2026-04-20 16:09:05 +0900

    Export marketplace adapters from packages

--- marketplace/buyma/__init__.py
+++ marketplace/buyma/__init__.py
@@ -1,1 +1,9 @@
 """BUYMA marketplace integration package."""
+
+from marketplace.buyma.mapper import BuymaRowMapper
+from marketplace.buyma.uploader import BuymaUploaderAdapter
+
+__all__ = [
+    "BuymaRowMapper",
+    "BuymaUploaderAdapter",
+]
